52db4c28d70959c3fdcb470bd5b2f45c9ba553e0
Benjamin Allan-Rahill <ballanrahill@middlebury.edu>
2020-12-16 17:18:43 -0500

    installing tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,16 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Link from "next/link";
+import Layout from "../components/Layout";
+import "tailwindcss/tailwind.css";
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
+    <h1 className="mx-40 blue">Hello Next.js 👋</h1>
     <p>
       <Link href="/about">
         <a>About</a>
       </Link>
     </p>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
